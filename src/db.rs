7f8b1d86584d4abb0676d24a474e97157b88ff05
Coleman McFarland <coleman.mcfarland@gmail.com>
2017-11-21 16:02:51 -0800

    to Err is human

--- src/db.rs
+++ src/db.rs
@@ -12,10 +12,10 @@ impl GetAs for DB {
     fn get_as<'a, T>(&self, key: &str) -> Result<T, String> where T: DeserializeOwned {
         match self.get(key.as_bytes()) {
             Ok(None) => Err(format!("Could not find key '{}' in DB", key)),
-            Ok(Some(db_vec)) => deserialize(&dbvec[..]).map_err(|e|
+            Ok(Some(db_vec)) => deserialize(&db_vec[..]).map_err(|e|
                 format!("Error deserializing key '{}' from DB: {:?}", key, e)
             ),
-            Err(e) => format!("Error fetching key '{}' from DB: {:?}", key, e),
+            Err(e) => Err(format!("Error fetching key '{}' from DB: {:?}", key, e)),
         }
     }
 }
