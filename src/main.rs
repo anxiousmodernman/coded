4fb80373f300636abb2996f4ce0a4e50faf8e2a9
Coleman McFarland <coleman.mcfarland@gmail.com>
2017-11-21 23:51:16 -0800

    remove dead code

--- src/main.rs
+++ src/main.rs
@@ -91,6 +91,3 @@ fn index(db: State<Arc<DB>>) -> String {
 }
 
 
-unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
-    ::std::slice::from_raw_parts((p as *const T) as *const u8, ::std::mem::size_of::<T>())
-}
