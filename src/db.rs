c135b14c32219b79b425b600c14407b72cdd55c5
Jeff May <jeff.n.may@gmail.com>
2017-11-21 22:06:58 -0500

    Use Higher-Rank Trait Bounds instead of Owned.

--- src/db.rs
+++ src/db.rs
@@ -1,15 +1,15 @@
 extern crate rocksdb;
 
-use serde::de::DeserializeOwned;
+use serde::de::Deserialize;
 use rocksdb::{DB, DBVector};
 use bincode::{deserialize, serialize, Infinite};
 
 pub trait GetAs {
-    fn get_as<'a, T>(&self, key: &str) -> Result<T, String> where T: DeserializeOwned;
+    fn get_as<T>(&self, key: &str) -> Result<T, String> where for <'a>  T: Deserialize<'a>;
 }
 
 impl GetAs for DB {
-    fn get_as<'a, T>(&self, key: &str) -> Result<T, String> where T: DeserializeOwned {
+    fn get_as<T>(&self, key: &str) -> Result<T, String> where for <'a> T: Deserialize<'a> {
         match self.get(key.as_bytes()) {
             Ok(None) => Err(format!("Could not find key '{}' in DB", key)),
             Ok(Some(db_vec)) => deserialize(&db_vec[..]).map_err(|e|
